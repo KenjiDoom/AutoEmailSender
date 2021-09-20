b098b8080bd74617d30bd3b90e421cc908e4d2dc
Kenjidoom <vitodevcool@protonmail.com>
2021-09-20 13:52:14 -0700

    added mins and seconds

--- prototypes/time_example.py
+++ prototypes/time_example.py
@@ -1,6 +1,5 @@
 import time
 import re
-import numpy as np
 
 def main():
 	print("""
@@ -17,11 +16,21 @@ def main():
 		convert_time = int(convert_number) # Converts the list into a integer
 		time.sleep(86400 * convert_time) # 86400 seconds equals one day 
 	elif 's' in user_time:
-		pass
+		print('Message will send in ' + user_time)
+		convert_number = "".join(re.findall(r'\d+', user_time))
+		convert_time = int(convert_number)
+		time.sleep(1 * convert_time)
 	elif 'm' in user_time:
-		pass
+		print('Message will be sent in ' + user_time)
+		convert_number = "".join(re.findall(r'\d+', user_time))
+		convert_time = int(convert_number)
+		time.sleep(60 * convert_time)
 	else:
 		print('Wrong input..')
+
+def calculate_time(user_time):
+	print(user_time)
+	return 0
 
 
 if __name__ == '__main__':
