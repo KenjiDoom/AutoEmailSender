e7b3501764480dc39df0c276c76383f8c741b2c8
Kenjidoom <vitodevcool@protonmail.com>
2021-09-20 17:12:34 -0700

    code changed, using class

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
+class AutoSender(object):
+	def main(self):
+		self.email = input('Email: ')
+		self.password = input('Password: ')
 
-def main():
-	print("1. Google")
-	print("2. Protonmail")
-	service = input("> ")
-	if service == 1 or 'google'.lower():
-		googleaccount()
-	elif service == 2 or 'protonmail'.lower():
-		protonaccount()
+	def wait_time(self):
+		self.time = input('Time: ')
 
-def googleaccount():
-	pass
-
-
-
-def protoaccount():
-	pass
+	def send(self):
+		self.main()
+		self.wait_time()
+		print(self.password)
+		print(self.time)
+		print(self.email)
 
 
-if __name__ == '__main__':
-	main()
+obj = AutoSender()
+obj.send()
