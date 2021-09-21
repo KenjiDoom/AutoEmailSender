d02bcd2c0eb3c07a4ca18d9b748579e4fdd4a5fb
Kenjidoom <vitodevcool@protonmail.com>
2021-09-21 15:25:37 -0700

    Made the script look pretty

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import time, os, re, sys
 
 class AutoSender(object):
 	def main(self):
+		print("""
+    _         _       ____                 _           
+   / \  _   _| |_ ___/ ___|  ___ _ __   __| | ___ _ __ 
+  / _ \| | | | __/ _ \___ \ / _ \ '_ \ / _` |/ _ \ '__|
+ / ___ \ |_| | || (_) |__) |  __/ | | | (_| |  __/ |   
+/_/   \_\__,_|\__\___/____/ \___|_| |_|\__,_|\___|_|   
+	Author @ github: kenjidoom
+""")
 		self.email = input('Email: ')
 		self.password = input('Password: ')
 		self.recv = input('Reciver: ')
@@ -15,12 +23,12 @@ class AutoSender(object):
 		try:
 			gmail_server.login(self.email, self.password)
 			resp = True
-			print('Password Correct')
+			print('[+] Password Correct!')
 			gmail_server.quit()
 		except:
 			resp = False
 			gmail_server.quit()
-			print('Inccorect Password')
+			print('[-] Inccorect Password!')
 			sys.exit()
 			return resp
 
@@ -33,35 +41,37 @@ class AutoSender(object):
 		s = seconds
 		""")
 		self.time = input('Time: ')
-		if 'd' in self.time:
-			print('Waiting ' + self.time)
+		if 'd' in self.time.lower():
+			print('[+] Waiting ' + self.time)
 			convert_number = "".join(re.findall(r'\d+', self.time))
 			convert_time = int(convert_number)
 			time.sleep(86400 * convert_time)
-		elif 'm' in self.time:
-			print('Waiting ' + self.time)
+		elif 'm' in self.time.lower():
+			print('[+] Waiting ' + self.time)
 			convert_number = "".join(re.findall(r'\d+', self.time))
 			convert_time = int(convert_number)
 			time.sleep(60 * convert_time)
-		elif 's' in self.time:
-			print('Waiting ' + self.time)
+		elif 's' in self.time.lower():
+			print('[+] Waiting ' + self.time)
 			convert_number = "".join(re.findall(r'\d+', self.time))
 			convert_time = int(convert_number)
 			time.sleep(1 * convert_time)
-
+		else:
+			print('[-] Wrong time input!')
+			sys.exit()
 
 	def send(self): # Sending email in the end
 		self.main()
 		self.wait_time()
 		os.system('clear')
 		port = 465
-		print('Sending message..')
+		print('[+] Sending message....')
 		# This will run after the time is done.
 		context = ssl.create_default_context()
 		with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
 			server.login(self.email, self.password)
 			server.sendmail(self.email, self.recv, self.subject)
-			print('Sent.')
+			print('[+] sent.')
 
 
 
