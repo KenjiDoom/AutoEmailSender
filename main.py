7fef418d5bb0e53b88ad3e75be611b84a351df36
kenjidoom <vitodevcool@protonmail.com>
2021-09-22 15:04:59 -0700

    Trying new method of sending email

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import smtplib, ssl
 import time, os, re, sys
+from email.message import EmailMessage
 
 class AutoSender(object):
 	def main(self):
 		print("""
-    _         _       ____                 _           
-   / \  _   _| |_ ___/ ___|  ___ _ __   __| | ___ _ __ 
+    _         _       ____                 _
+   / \  _   _| |_ ___/ ___|  ___ _ __   __| | ___ _ __
   / _ \| | | | __/ _ \___ \ / _ \ '_ \ / _` |/ _ \ '__|
- / ___ \ |_| | || (_) |__) |  __/ | | | (_| |  __/ |   
-/_/   \_\__,_|\__\___/____/ \___|_| |_|\__,_|\___|_|   
+ / ___ \ |_| | || (_) |__) |  __/ | | | (_| |  __/ |
+/_/   \_\__,_|\__\___/____/ \___|_| |_|\__,_|\___|_|
 	Author @ github: kenjidoom
 """)
 		self.email = input('Email: ')
@@ -17,7 +18,18 @@ class AutoSender(object):
 		self.subject = input('Subject: ')
 		self.message = input('Message: ')
 		self.attachment = input('Attachment: ')
-		
+		# self.final_message = """
+		# Subject: {}
+		#
+		# {}
+		# """.format(self.subject, self.message)
+		self.final_message_again = EmailMessage()
+		self.final_message_again.message_content(self.message)
+		self.final_message_again['Subject'] = self.subject
+		self.final_message_again['From'] = self.email
+		self.final_message_again['To'] = self.recv
+
+
 		gmail_server = smtplib.SMTP('smtp.gmail.com:587')
 		gmail_server.starttls()
 		try:
@@ -70,8 +82,8 @@ class AutoSender(object):
 		context = ssl.create_default_context()
 		with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
 			server.login(self.email, self.password)
-			server.sendmail(self.email, self.recv, self.subject)
-			print('[+] sent.')
+			server.send_message(self.final_message_again)
+			print('[+] Sent.')
 
 
 
